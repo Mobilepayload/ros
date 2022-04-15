60200f1261d19ded5f1a72d298b7d28a4f27c034
Mobilepayload <95688210+Mobilepayload@users.noreply.github.com>
2022-04-15 15:07:33 +0530

    Add files via upload

---
+++ gesture/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetson/catkin_ws/src/gesture
+# Build directory: /home/jetson/catkin_ws/build/gesture
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
